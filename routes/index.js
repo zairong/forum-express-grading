d21ecbc6550443bdb952cfdec5c07a0b3d506207
zairong <justiceman3211@hotmail.com>
2023-01-16 14:45:48 +0800

    add index&admin page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const restController = require('../controllers/restaurant-controller')
+const admin = require('./modules/admin')
 
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+router.use('/admin', admin)
 
+router.get('/restaurants', restController.getRestaurants)
+
+// 如無匹配的路由跳轉至/restaurants
+router.use('/', (req, res) => res.redirect('/restaurants'))
 module.exports = router
