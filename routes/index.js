25ef35fb8bab51cf1b1e285dc4b07792dffe7690
zairong <justiceman3211@hotmail.com>
2023-01-17 11:20:04 +0800

    add signin and signup page

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const router = express.Router()
-const restController = require('../controllers/restaurant-controller')
 const admin = require('./modules/admin')
+const restController = require('../controllers/restaurant-controller')
+const userController = require('../controllers/user-controller')
+const { generalErrorHandler } = require('../middleware/error-handler')
+const passport = require('../config/passport') // 引入 Passport，需要他幫忙做驗證
 
 router.use('/admin', admin)
-
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp) // 注意用 post
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn) // 注意是 post
 router.get('/restaurants', restController.getRestaurants)
-
-// 如無匹配的路由跳轉至/restaurants
-router.use('/', (req, res) => res.redirect('/restaurants'))
+router.get('/', (req, res) => res.redirect('/restaurants'))
+router.use('/', generalErrorHandler)
 module.exports = router
